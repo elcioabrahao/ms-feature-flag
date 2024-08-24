18091a2718f02cf32ef26f8ef1ed4370a92b579c
elcio <elcioabrahao@alumni.usp.br>
2024-08-24 11:17:51 -0300

    Pequenos ajustes

--- src/main/java/br/com/alfa11/msfeatureflag/model/ResponseEntity.java
+++ src/main/java/br/com/alfa11/msfeatureflag/model/ResponseEntity.java
@@ -1,5 +1,6 @@
 package br.com.alfa11.msfeatureflag.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +13,20 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class ResponseEntity {
+    @Schema(name="applicationId", example="web01", description = "Identificado da aplicação corrente (conforme criado no registro de feature)")
     private String applicationId;
+    @Schema(name="userId", example="KSFSKFHKSFSFHKSFH", description = "Identificador do usuário (mesmo formato da lista de allow all/denny all)")
     private String userId;
+    @Schema(name="companyId", example="43098758000179", description = "Identificador de empresa (mesmo formato da lista de allow all/denny all)")
     private String companyId;
+    @Schema(name="filterApplied", example="Lista de Permitidos", description = "Nome do filtro que vai ser aplicado a respectiva funcionalidade")
     private boolean filterApplied;
+    @Schema(name="filterAppliedIds", example="", description = "Lista dos IDs dos filtros aplicados")
     private List<String> filterAppliedIds;
+    @Schema(name="featureIds", example="", description = "Lista dos IDs das funcionlidades que podem ser executadas/exibidas. Somente as permitidas aparecem nessa lista, se a aplicação cliente não encontrar o ID aqui então deve negar a execussão/exibição da funcionalidade.")
     private List<String> featureIds;
+    @Schema(name="success", example="true", description = "True se a solicitação foi bem scessidade false em contrário")
     private boolean success;
+    @Schema(name="errorMessage", example="Erro no serviço", description = "Mensagem de errro é exibida em caso de sucess FALSE.")
     private String errorMessage;
 }
